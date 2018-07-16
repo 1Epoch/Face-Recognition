22efa73bc6f2727b7145f647dcbf2b4a4ea003ec
Keshav Aditya R.P <keshavaditya26896@gmail.com>
2018-07-16 14:49:01 +0530

    Face Recognition using Triple Loss Function

--- tripleLossFaceNet/fr_utils.py
+++ tripleLossFaceNet/fr_utils.py
@@ -1,5 +1,4 @@
 #### PART OF THIS CODE IS USING CODE FROM VICTOR SY WANG: https://github.com/iwantooxxoox/Keras-OpenFace/blob/master/utils.py ####
-#### THIS FILE IS FROM https://github.com/shahariarrabby/deeplearning.ai/blob/master/COURSE%204%20Convolutional%20Neural%20Networks/Week%2004/Face%20Recognition/fr_utils.py
 
 import tensorflow as tf
 import numpy as np
@@ -189,14 +188,9 @@ def load_dataset():
     
     return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
 
-def img_path_to_encoding(image_path, model):
+def img_to_encoding(image_path, model):
     img1 = cv2.imread(image_path, 1)
-    return img_to_encoding(img1, model)
-    
-
-def img_to_encoding(image, model):
-    image = cv2.resize(image, (96, 96)) 
-    img = image[...,::-1]
+    img = img1[...,::-1]
     img = np.around(np.transpose(img, (2,0,1))/255.0, decimals=12)
     x_train = np.array([img])
     embedding = model.predict_on_batch(x_train)
